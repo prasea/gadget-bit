2729cfe16d43c49971e7052ce021a642b6cd23ff
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 07:08:53 +0545

    Create edit, update, destroy action method and view for admin category

--- gadget-bit/app/controllers/admin/categories_controller.rb
+++ gadget-bit/app/controllers/admin/categories_controller.rb
@@ -20,6 +20,25 @@ class Admin::CategoriesController < AdminController
     end
   end
 
+  def edit 
+    @category = Category.find(params[:id])
+  end
+
+  def update 
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_category_path(@category), notice: 'Category successfully updated'
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @category = Category.find(params[:id])
+    @category.destroy!
+    redirect_to admin_categories_path, status: :see_other
+  end
+  
   private 
   def category_params
     params.require(:category).permit(:name, :description)
