4cca107e5d68cb4a23bd6f7f60dbf5e53298f8e3
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 16:40:52 +0545

    feat: List product added by admin to customers

--- gadget-bit/config/routes.rb
+++ gadget-bit/config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       resources :images, only: :destroy
     end
   end
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
