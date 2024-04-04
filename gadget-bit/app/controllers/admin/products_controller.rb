6a5eaf01fc235bac6c7fe231cce604c631b0b9b7
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 09:30:08 +0545

    feat: Add functionality to add new product by manually typing the category_id and show added product

--- gadget-bit/app/controllers/admin/products_controller.rb
+++ gadget-bit/app/controllers/admin/products_controller.rb
@@ -2,4 +2,26 @@ class Admin::ProductsController < AdminController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new 
+  end
+
+  def create 
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: 'Product successfullly created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show 
+    @product = Product.find(params[:id])
+  end
+
+  private 
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category_id)
+  end
 end
