7c6ac2ba458a0d323ec2461ee0ca58967c8175ed
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 09:14:54 +0545

    feat: Generate product model, create admin's products routes and controller with index action method

--- gadget-bit/db/schema.rb
+++ gadget-bit/db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_204811) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_031926) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 8, scale: 2
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_204811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
