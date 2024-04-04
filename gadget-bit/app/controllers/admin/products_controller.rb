22a94e3cc5aca43a1024fbcb7e2e2491ba71444f
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 10:30:00 +0545

    feat: Add before_action callback to set_product in products controller

--- gadget-bit/app/controllers/admin/products_controller.rb
+++ gadget-bit/app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < AdminController
+  before_action :set_product, only: %i[show edit update destroy]
   def index
     @products = Product.all
   end
@@ -17,15 +18,12 @@ class Admin::ProductsController < AdminController
   end
 
   def show 
-    @product = Product.find(params[:id])
   end
 
   def edit 
-    @product = Product.find(params[:id])
   end
 
   def update 
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to admin_product_path(@product), notice: 'Product successfully updated'
     else 
@@ -34,7 +32,6 @@ class Admin::ProductsController < AdminController
   end
 
   def destroy 
-    @product = Product.find(params[:id])
     @product.destroy!
     redirect_to admin_products_path, status: :see_other
   end
@@ -43,5 +40,9 @@ class Admin::ProductsController < AdminController
   private 
   def product_params
     params.require(:product).permit(:name, :description, :price, :category_id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
