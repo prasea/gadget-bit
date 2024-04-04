5dd4d5cbff6dac32918ec7f461db0f74d46f853b
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 06:57:52 +0545

    feat: Create show, edit and create category action method and view

--- gadget-bit/app/controllers/admin/categories_controller.rb
+++ gadget-bit/app/controllers/admin/categories_controller.rb
@@ -1,5 +1,27 @@
 class Admin::CategoriesController < AdminController
   def index 
     @categories = Category.all
-  end 
+  end
+
+  def show 
+    @category = Category.find(params[:id])
+  end
+
+  def new 
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save 
+      redirect_to admin_category_path(@category), notice: 'Category successfully created.'
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end 
