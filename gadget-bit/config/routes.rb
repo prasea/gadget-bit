bbab3c71e4a8ee654a6c1a5c08e3ef32065c6ec3
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 14:43:00 +0545

    feat: Admin user can remove the image from the edit view of product

--- gadget-bit/config/routes.rb
+++ gadget-bit/config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
   namespace :admin do 
     resources :categories
-    resources :products
+    resources :products do 
+      resources :images, only: :destroy
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
