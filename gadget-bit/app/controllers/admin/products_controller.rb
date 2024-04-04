ff92be4a6ac7b4ededde64244c9b3ee93ba4ee60
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-04 09:39:46 +0545

    feat: Add functionality to edit product for admin user

--- gadget-bit/app/controllers/admin/products_controller.rb
+++ gadget-bit/app/controllers/admin/products_controller.rb
@@ -20,6 +20,19 @@ class Admin::ProductsController < AdminController
     @product = Product.find(params[:id])
   end
 
+  def edit 
+    @product = Product.find(params[:id])
+  end
+
+  def update 
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: 'Product successfully updated'
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private 
   def product_params
     params.require(:product).permit(:name, :description, :price, :category_id)
