b00eb8b73eeffa26269c19c59d8c4096196ef6fe
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-05 06:56:58 +0545

    feat: Create carts show route and link to it from cart icon in nav

--- gadget-bit/config/routes.rb
+++ gadget-bit/config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     end
   end
   resources :products
+
+  get '/carts/:id', to: 'carts#show', as: 'carts'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
