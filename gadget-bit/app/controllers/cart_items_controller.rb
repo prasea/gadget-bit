d9940b0f183d819b204955c2128b046dbb6dcb59
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-07 08:11:46 +0545

    feat: Use Hotwire to implement add or decrease quantity via POST only

--- gadget-bit/app/controllers/cart_items_controller.rb
+++ gadget-bit/app/controllers/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!, only: :add_to_cart
-  
+
   def buy_now 
     find_or_create_cart_item
     @cart_item.save
@@ -24,7 +24,6 @@ class CartItemsController < ApplicationController
     end
   end
 
-
   def remove_from_cart 
     @selected_product = Product.find(params[:product_id])
     @cart_item = @current_cart.cart_items.find_by(product_id: @selected_product)
@@ -38,9 +37,18 @@ class CartItemsController < ApplicationController
     if @cart_item.product.stock.quantity > @cart_item.quantity
       @cart_item.quantity += 1
       @cart_item.save
-      redirect_to carts_path(@current_cart), notice: 'Quantity incremented successfully.'      
+      # redirect_to carts_path(@current_cart), notice: 'Quantity incremented successfully.'      
+      respond_to do |format| 
+        format.turbo_stream { render turbo_stream: [
+          turbo_stream.update("quantity_#{@cart_item.id}", partial: "carts/quantity", locals: {cart_item: @cart_item}),
+          turbo_stream.replace("flash_messages", partial: "carts/flash_messages", locals: { alert: 'Quantity incremented successfully.', alert_type: 'success' })
+        ]}
+      end
     else
-      redirect_to carts_path(@current_cart), alert: 'Cannot increment quantity further. Product stock limit reached.'
+      # redirect_to carts_path(@current_cart), alert: 'Cannot increment quantity further. Product stock limit reached.'
+      respond_to do |format| 
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash_messages", partial: "carts/flash_messages", locals: { alert: 'Cannot increment quantity further. Product stock limit reached.', alert_type: 'danger' }) }
+      end
     end
   end 
 
@@ -50,9 +58,18 @@ class CartItemsController < ApplicationController
     if @cart_item.quantity > 1
       @cart_item.quantity -= 1
       @cart_item.save
-      redirect_to carts_path(@current_cart), notice: 'Quantity decremented successfully.'
+      # redirect_to carts_path(@current_cart), notice: 'Quantity decremented successfully.'
+      respond_to do |format| 
+        format.turbo_stream { render turbo_stream: [
+          turbo_stream.update("quantity_#{@cart_item.id}", partial: "carts/quantity", locals: {cart_item: @cart_item}),
+          turbo_stream.replace("flash_messages", partial: "carts/flash_messages", locals: { alert: 'Quantity decremented successfully.', alert_type: 'success' })
+        ]}
+      end
     else
-      redirect_to carts_path(@current_cart), alert: 'Cannot decrement quantity further. Minimum quantity reached.'
+      # redirect_to carts_path(@current_cart), alert: 'Cannot decrement quantity further. Minimum quantity reached.'
+      respond_to do |format| 
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash_messages", partial: "carts/flash_messages", locals: { alert: 'Cannot decrement quantity further. Minimum quantity reached.', alert_type: 'danger' }) }
+      end
     end
   end 
   
