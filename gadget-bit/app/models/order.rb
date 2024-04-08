f3f331af12eb3712538e4cabe890230a1a73f5a9
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-08 14:32:16 +0545

    feat: Calculate the total price of each product with quantity for order

--- gadget-bit/app/models/order.rb
+++ gadget-bit/app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_one :order_address, dependent: :destroy
+
+  def total 
+    sum = 0 
+    cart_items.each do |cart_item|
+      sum += cart_item.total_of_single_item
+    end
+    return sum 
+   end
 end
--- gadget-bit/app/models/order_address.rb
+++ gadget-bit/app/models/order_address.rb
@@ -1,3 +1,4 @@
 class OrderAddress < ApplicationRecord
   belongs_to :order
+  NEPAL_STATES = ['Koshi Province', 'Madhesh Province', 'Bagmati Province', 'Gandaki Province', 'Lumbini Province', 'Karnali Province', 'Sudurpashchim Province']
 end
