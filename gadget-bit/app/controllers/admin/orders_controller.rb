94e13f771f1b0ad5d27a16377a6fc18ee6b5aef7
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-08 16:36:16 +0545

    feat: Admin user can view all fulfilled and unfulfilled orders

--- gadget-bit/app/controllers/admin/orders_controller.rb
+++ gadget-bit/app/controllers/admin/orders_controller.rb
@@ -1,9 +1,11 @@
 class Admin::OrdersController < AdminController 
   def index 
-    @orders = Order.all
+    # @orders = Order.all
+    @not_fulfilled_orders = Order.where(fulfilled: false).order(created_at: :asc)
+    @fulfilled_orders = Order.where(fulfilled: true).order(created_at: :asc)
   end
 
   def show 
-    @order = Order.find(params[:id])    
+    @order = Order.find(params[:id])        
   end
 end
