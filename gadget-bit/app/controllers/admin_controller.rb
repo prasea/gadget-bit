325dec929d5567ef6ee4e11541e591c6e4981075
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-09 01:04:24 +0545

    feat: Display latest 5 unfulfulfilled orders and intialized chart.js

--- gadget-bit/app/controllers/admin_controller.rb
+++ gadget-bit/app/controllers/admin_controller.rb
@@ -5,9 +5,9 @@ class AdminController < ApplicationController
     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
     @quick_stats = {
       sales: Order.where(created_at: Time.now.midnight..Time.now).count, 
-      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(), 
-      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(), 
-      total_products_sold: Order.joins(:cart_items).where(created_at: Time.zone.now.all_day).sum('cart_items.quantity')
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).try(:round), 
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).try(:round), 
+      total_products_sold: Order.joins(:cart_items).where(created_at: Time.now.midnight..Time.now).sum('cart_items.quantity')
     } 
   end
 
