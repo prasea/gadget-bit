e338124d54b8d89c4612d675d876e886f14ac98a
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-08 23:29:23 +0545

    feat: Fetch and display daily avg sale and total product quantity sold in admin index

--- gadget-bit/app/controllers/admin_controller.rb
+++ gadget-bit/app/controllers/admin_controller.rb
@@ -5,7 +5,9 @@ class AdminController < ApplicationController
     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
     @quick_stats = {
       sales: Order.where(created_at: Time.now.midnight..Time.now).count, 
-      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round()
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(), 
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(), 
+      total_products_sold: Order.joins(:cart_items).where(created_at: Time.zone.now.all_day).sum('cart_items.quantity')
     } 
   end
 
