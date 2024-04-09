4837d691c7de878304826ef22fba295518457fa3
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-09 11:43:46 +0545

    feat: User can clear the search and must enter query param to search

--- gadget-bit/app/controllers/products_controller.rb
+++ gadget-bit/app/controllers/products_controller.rb
@@ -11,7 +11,12 @@ class ProductsController < ApplicationController
     @buy_now_method = current_user ? :post : :get
   end
 
-  def search 
-    @products = Product.where("name ILIKE ?", "%" + params[:q] + "%")
-  end 
+  def search
+    if params[:q].present?
+      @products = Product.where("name ILIKE ?", "%" + params[:q] + "%")
+    else
+      flash[:notice] = "Please enter a search query."
+      redirect_to products_path
+    end
+  end
 end 
