fcac18fd79a9235272ac824fbc33ea1cb3ef0532
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-09 13:55:27 +0545

    feat: Use price-filter stimulus controller for field validation and prevent submit
    Also removed the inclusion of chart js cdn

---
+++ gadget-bit/app/javascript/controllers/price_filter_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  validate(event) {
+    const minPriceInput = document.querySelector("#min_price");
+    const maxPriceInput = document.querySelector("#max_price");
+
+    const minPrice = minPriceInput.value.trim();
+    const maxPrice = maxPriceInput.value.trim();
+
+    if (minPrice === '' || maxPrice === '' || isNaN(minPrice) || isNaN(maxPrice) || parseFloat(minPrice) < 0 || parseFloat(maxPrice) < 0) {
+      const alertDiv = document.createElement('div');
+      alertDiv.classList.add('alert', 'alert-danger');
+      alertDiv.textContent = 'Please enter valid prices.';
+      
+      const form = document.querySelector('form[data-controller="price-filter"]');
+      const parentElement = form.parentElement;
+      parentElement.insertBefore(alertDiv, form);
+
+      setTimeout(() => {
+        alertDiv.remove();
+      }, 3000);
+
+      event.preventDefault(); 
+    }
+  }
+}
