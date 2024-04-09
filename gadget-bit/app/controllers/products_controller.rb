cd428e33c57be8a000cf24119312629da53d0498
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-09 15:21:20 +0545

    feat: Display message when searched product not found

--- gadget-bit/app/controllers/products_controller.rb
+++ gadget-bit/app/controllers/products_controller.rb
@@ -21,9 +21,13 @@ class ProductsController < ApplicationController
   def search
     if params[:q].present?
       @products = Product.where("name ILIKE ?", "%" + params[:q] + "%")
+      if @products.empty?
+        flash.now[:notice] = "No products found for '#{params[:q]}'."
+      end
     else
       flash[:notice] = "Please enter a search query."
       redirect_to products_path
     end
   end
+  
 end 
