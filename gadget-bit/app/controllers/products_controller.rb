60bdb118bc2c52d8afaa908f94346533f19a3306
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-09 13:01:42 +0545

    feat: User can filter the products by price
    The price filter is applied on products index view as well as categories show view

--- gadget-bit/app/controllers/products_controller.rb
+++ gadget-bit/app/controllers/products_controller.rb
@@ -3,6 +3,13 @@ class ProductsController < ApplicationController
   def index 
     @products = Product.all
     @buy_now_method = current_user ? :post : :get
+
+    if params[:max_price].present?
+      @products = @products.where("price <= ?", params[:max_price])     
+    end
+    if params[:min_price].present?
+      @products = @products.where("price >= ?", params[:min_price])     
+    end
   end
 
   def show 
