d539dd9352b5c5cf85fab42dd261008783c434b5
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-10 16:16:49 +0545

    feat: Uniqueness model validation for category name

--- gadget-bit/app/models/category.rb
+++ gadget-bit/app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 80 }
+  validates :name, presence: true, length: { maximum: 80 }, uniqueness: true
   validates :description, presence: true, length: {maximum: 500}
   has_many :products, dependent: :destroy
 end
