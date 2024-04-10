251fc7bc96cc8d5c41d9636ed1a1729e0443f7b6
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-11 05:07:52 +0545

    feat: Validate stock of product adding form by admin using model validation

--- gadget-bit/app/models/stock.rb
+++ gadget-bit/app/models/stock.rb
@@ -1,3 +1,4 @@
 class Stock < ApplicationRecord
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   belongs_to :product, dependent: :destroy
 end
