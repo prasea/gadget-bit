4e739d42316865f3353b9353651ccd825e42ea18
prajanya <parajanya.shrestha@bajratechnologies.com>
2024-04-11 09:46:20 +0545

    feat: Make the login and sign up form attractive

--- gadget-bit/app/models/user.rb
+++ gadget-bit/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :contact, presence: true
+  validates :contact, presence: true, format: { with: /\A\d{10}\z/, message: "must be a 10-digit number" }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :cart, dependent: :destroy
